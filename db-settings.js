7f038e08c25ddc7c1942b99a0dc6e8d24e4b9b67
Julia_Vladomira <juliavladomira@gmail.com>
2023-06-30 17:59:55 +0200

    db settings were changede to simple version

--- db-settings.js
+++ db-settings.js
@@ -1,19 +1,4 @@
 const { Sequelize } = require("sequelize");
 const { pg } = require("pg");
 
-module.exports = new Sequelize(
-   process.env.PGDATABASE,
-   process.env.PGUSER,
-   process.env.PGPASSWORD,
-   {
-      dialect: "postgres",
-      // dialectModule: pg,
-      PORT: 5432,
-      host: process.env.PGHOST,
-      dialectOptions: {
-         ssl: {
-            rejectUnauthorized: false,
-         },
-      },
-   }
-);
+module.exports = new Sequelize(process.env.DATABASE_URL);
