7b3e0db0febeca1b9e72821b989a4f8d746a3db9
Julia_Vladomira <juliavladomira@gmail.com>
2023-06-30 13:58:59 +0200

    add database url

--- db-settings.js
+++ db-settings.js
@@ -1,15 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { pg } = require("pg");
 
-module.exports = new Sequelize(
-   process.env.DB_NAME_DVPLMNT,
-   process.env.DB_USER_DVPLMNT,
-   process.env.DB_PASSWORD_DVPLMNT,
-   {
-      dialect: "postgres",
-      user: "root",
-      password: "science",
-      host: process.env.DB_HOST_DVPLMNT || "localhost",
-      PORT: process.env.DB_PORT_DVPLMNT,
-   }
-);
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+   dialectModule: pg,
+});
