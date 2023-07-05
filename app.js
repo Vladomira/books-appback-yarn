71c4f2c7eb282eb37f9296f327178d95d9b2bbd6
Julia_Vladomira <juliavladomira@gmail.com>
2023-07-05 18:10:08 +0200

    change cors  settings

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ router.use(bodyParser.json());
 app.use(cookieParser());
 
 const corsOptions = {
-   // origin: "http://localhost:3000",
+   origin: "http://localhost:3000",
    credentials: true,
    optionSuccessStatus: 200,
 };
